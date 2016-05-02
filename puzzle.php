079ee749987982729ad0235a1173017c1ec78fb8
devileya <arif14si@mahasiswa.pcr.ac.id>
2016-05-03 01:07:13 +0700

    Update puzzle.php

--- puzzle.php
+++ puzzle.php
@@ -35,8 +35,8 @@
 	
 </head>
 <header style="height: 90px;">
-    <table><tr><td><h1  align="center" >Nama : <?php print $nama;?></h1></td><td><h1 align="center" style="background-color: black">Waktu:<span id="countdown"></span><h1></td><td><h1 align="right">Poin : <?php print $pemain[1];?></h1></td></tr></table></header>
 <body style="background-image:url(image/background1.jpg);background-position:center; background-size: 100% 1200px ">
+    <table><tr><td><h1  align="center" >Nama : <?php print $nama;?></h1></td><td><h1 align="center" style="background-color: black">Waktu:<span id="countdown"></span><h1></td><td><h1 align="right">Poin : <?php print $pemain[1];?></h1></td></tr></table></header>
     
     <span id="idgam" hidden="true"><?php print $pz[0];?></span>
     <span id="nama" hidden="true"><?php print $nama;?></span>
